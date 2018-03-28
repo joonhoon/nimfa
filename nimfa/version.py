7ab6247c08ec25068d40ba14d408df658d6a643c
Joonhoon Kim <joonhoon.kim@gmail.com>
2018-03-28 13:08:20 -0700

    ONMF working commit

---
+++ nimfa/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM NIMFA SETUP.PY
+short_version = '1.3.4'
+version = '1.3.4'
+full_version = '1.3.4'
+git_revision = 'dc0e55340e0e469fc93ffb0ba81a1abd5257afbc'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
